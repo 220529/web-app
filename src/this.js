17d7d553943c1e74bd28cfb8fb1abaae3cba6f5e
liukaixin <15713857919@163.com>
2024-08-30 14:17:09 +0800

    test1

--- src/this.js
+++ src/this.js
@@ -2,11 +2,11 @@ var name = "kkk";
 const obj = {
   name: "Alice",
   greet() {
-    console.log("greet: ", this.name);
+    console.log("greet1: ", this.name);
   },
   say: () => {
     console.log("say: ", this.name);
-  }
+  },
 };
 obj.greet();
 obj.say();
