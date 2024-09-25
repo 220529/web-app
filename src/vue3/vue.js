e90f1dce87a6af8bc6218150b402a37df9a2bbfc
liukaixin <15713857919@163.com>
2024-09-25 11:17:57 +0800

    test

--- src/vue3/vue.js
+++ src/vue3/vue.js
@@ -17,14 +17,18 @@ const tarck = (target, key) => {
 
 const trigger = (target, key) => {
   const depMap = effectMap.get(target);
-  const deps = depMap.get(key);
-  deps.forEach(fn => fn());
+  if (depMap) {
+    const deps = depMap.get(key);
+    deps?.forEach(fn => fn());
+  }
 };
 
 const handler = {
   get(target, key) {
+    // console.log("handler", target, key);
     tarck(target, key);
-    return Reflect.get(...arguments);
+    const result = Reflect.get(...arguments);
+    return typeof result === "object" ? _reactive(result) : result;
   },
   set(target, key, value) {
     if (target[key] !== value) {
@@ -47,16 +51,19 @@ const Vue = { reactive: _reactive, effect: _effect };
 
 const { reactive, effect } = Vue;
 
-let data = reactive({ message: "Hello Vue 3" });
+let data = reactive({ message: "Hello Vue 3", obj: { a: 1 } });
 
+// effect(() => {
+//   console.log("effect.message", data.message); // 依赖收集
+// });
 effect(() => {
-  console.log("effect", data.message); // 依赖收集
+  console.log("effect.obj", data.obj); // 依赖收集
 });
 
-effect(() => {
-  console.log("effect2", data.message); // 依赖收集
-});
+// data.message = "Hello World"; // 触发更新
+// data.obj = { a: 1 };
 
-data.message = "Hello World"; // 触发更新
+// console.log("effectMap", effectMap);
 
-console.log("effectMap", effectMap);
+// data.obj = { x: 100 };
+data.obj.x = 200;
